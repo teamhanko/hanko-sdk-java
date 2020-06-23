f7197ad807fc7d0e707b69a78808afadea0015e8
Frederic Jahn <frederic.jahn@hanko.io>
2020-06-23 13:51:49 +0200

    remove userVerification from top level

--- src/main/java/io/hanko/sdk/models/CreateWebAuthnRequest.java
+++ src/main/java/io/hanko/sdk/models/CreateWebAuthnRequest.java
@@ -1,28 +1,30 @@
 package io.hanko.sdk.models;
 
+/**
+ * Class which holds all relevant data to request an web authentication operation.
+ */
 public class CreateWebAuthnRequest extends CommonCreateRequest {
     private AuthenticatorSelectionCriteria authenticatorSelectionCriteria;
-    private UserVerification userVerificationRequirement;
     private String displayName;
 
+    /**
+     * Set the AuthenticatorSelectionCriteria to specify which type of authenticator you allow for the operation
+     * @param authenticatorSelectionCriteria
+     */
     public void setAuthenticatorSelectionCriteria(AuthenticatorSelectionCriteria authenticatorSelectionCriteria) {
         this.authenticatorSelectionCriteria = authenticatorSelectionCriteria;
     }
 
-    public void setUserVerificationRequirement(io.hanko.sdk.models.UserVerification userVerificationRequirement) {
-        this.userVerificationRequirement = userVerificationRequirement;
-    }
-
+    /**
+     * Set the displayName which will be included in the request
+     * @param displayName
+     */
     public void setDisplayName(String displayName) {
         this.displayName = displayName;
     }
 
     public AuthenticatorSelectionCriteria getAuthenticatorSelectionCriteria() {
         return authenticatorSelectionCriteria;
-    }
-
-    public UserVerification getUserVerificationRequirement() {
-        return userVerificationRequirement;
     }
 
     public String getDisplayName() {
